a07ce5662c683f2d248c23778f1b39a98ed10d7b
Barry Chen <barry.chen@oocl.com>
2019-07-26 22:34:42 +0800

    chore: init project

---
+++ src/main/java/com/alfred/parkingalfred/ParkingAlfredBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alfred.parkingalfred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingAlfredBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkingAlfredBackendApplication.class, args);
+    }
+
+}
