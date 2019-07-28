885f7eb96629acbf7c15448de61509f319b4f303
zhanggi3 <gio.zhang@oocl.com>
2019-07-28 14:07:12 +0800

    [gio]:[feat,test feat EmployeeController's getParkingLotsByEmployeeId and add util ResultVOUtil]

--- src/main/java/com/alfred/parkingalfred/controller/EmployeeController.java
+++ src/main/java/com/alfred/parkingalfred/controller/EmployeeController.java
@@ -1,28 +1,46 @@
 package com.alfred.parkingalfred.controller;
 
 import com.alfred.parkingalfred.entity.Employee;
+import com.alfred.parkingalfred.entity.ParkingLot;
+import com.alfred.parkingalfred.enums.ResultEnum;
 import com.alfred.parkingalfred.service.EmployeeService;
+import com.alfred.parkingalfred.service.ParkingLotService;
 import com.alfred.parkingalfred.utils.JwtUtil;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import com.alfred.parkingalfred.vo.ResultVO;
+import com.alfred.parkingalfred.utils.ResultVOUtil;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    private final EmployeeService employeeService;
+  private final EmployeeService employeeService;
 
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+  private final ParkingLotService parkingLotService;
+
+  public EmployeeController(EmployeeService employeeService, ParkingLotService parkingLotService) {
+    this.employeeService = employeeService;
+    this.parkingLotService = parkingLotService;
+  }
+
+  @GetMapping(value = "/login", params = {"name", "password"})
+  public ResultVO<Object> login(@RequestParam String name, @RequestParam String password) {
+    Employee employee = employeeService.getEmployeeByNameAndPassword(name, password);
+    if (employee == null) {
+      return ResultVOUtil.error(ResultEnum.EMPLOYEE_NOT_EXISTED.getStatus()
+          , ResultEnum.EMPLOYEE_NOT_EXISTED.getMessage());
     }
+    String token = JwtUtil.generateToken(employee);
+    return ResultVOUtil.success(token);
+  }
 
-    @GetMapping(value = "/login", params = {"name", "password"})
-    public ResponseEntity login(@RequestParam String name, @RequestParam String password) {
-        Employee employee = employeeService.getEmployeeByNameAndPassword(name, password);
-        if (employee == null)
-            return ResponseEntity.notFound().build();
-        String token = JwtUtil.generateToken(employee);
-        return ResponseEntity.ok(token);
-    }
-
+  @GetMapping(value = "/employee/{employeeId}/parking-lots/")
+  public ResultVO<List<ParkingLot>> getParkingLotsByEmployeeId(@PathVariable Long employeeId) {
+    List<ParkingLot> parkingLots = parkingLotService.getParkingLotsByParkingBoyId(employeeId);
+    return  ResultVOUtil.success(parkingLots);
+  }
 
 }
--- src/test/java/com/alfred/parkingalfred/controller/EmployeeControllerTest.java
+++ src/test/java/com/alfred/parkingalfred/controller/EmployeeControllerTest.java
@@ -1,10 +1,15 @@
 package com.alfred.parkingalfred.controller;
 
 import com.alfred.parkingalfred.entity.Employee;
+import com.alfred.parkingalfred.entity.ParkingLot;
 import com.alfred.parkingalfred.enums.RoleEnum;
 import com.alfred.parkingalfred.service.EmployeeService;
+import com.alfred.parkingalfred.service.ParkingLotService;
+import com.alfred.parkingalfred.utils.JwtUtil;
+import java.util.Arrays;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,7 +30,8 @@ public class EmployeeControllerTest {
 
     @MockBean
     private EmployeeService employeeService;
-
+    @MockBean
+    private ParkingLotService parkingLotService;
     @Autowired
     private MockMvc mockMvc;
 
@@ -56,5 +62,22 @@ public class EmployeeControllerTest {
                 .param("name", name)
                 .param("password", password))
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void should_return_parkingLots_when_call_getParkingLotsByEmployeeId_API_with_true_param()
+        throws Exception {
+        Long employeeId= 1L;
+        ParkingLot parkingLot = new ParkingLot((long) 1,"test lot",100,100);
+
+        Employee employee = new Employee();
+        employee.setId(employeeId);
+        String token = JwtUtil.generateToken(employee);
+
+        when(parkingLotService.getParkingLotsByParkingBoyId(employeeId)).thenReturn(Arrays.asList(parkingLot));
+
+        mockMvc.perform(get("/employee/{employeeId}/parking-lots/",employeeId)
+        .header("Authorization", "Bearer " + token))
+            .andExpect(status().isOk());
     }
 }
