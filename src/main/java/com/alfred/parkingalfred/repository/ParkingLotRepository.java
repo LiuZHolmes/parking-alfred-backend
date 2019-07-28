eb85a3aadade610c56a632a6af2e827ed76c6119
zhanggi3 <gio.zhang@oocl.com>
2019-07-28 12:10:32 +0800

    [gio]:[feat,add  parkingLotRepository ]

---
+++ src/main/java/com/alfred/parkingalfred/repository/ParkingLotRepository.java
@@ -0,0 +1,8 @@
+package com.alfred.parkingalfred.repository;
+
+import com.alfred.parkingalfred.entity.ParkingLot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParkingLotRepository extends JpaRepository<ParkingLot,Long> {
+
+}
